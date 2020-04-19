ebd166532b8c610a8e7ca3db585281186961ada5
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-04-19 16:01:40 +0300

    tasarımsal revizeler

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,13 +17,19 @@ class HomeController extends Controller
             ->take(5);
 
 
-        $lastBlog = Blog::orderByDesc('id')->where('status', 1)->first();
+        $lastBlog = Blog::orderByDesc('id')->where('status', 1)
+            ->withCount(['comments' => function($q) {
+                $q->where('status', 1);
+            }])
+            ->first();
 
         $blogs = Blog::where('status', 1)
             ->orderByDesc('id');
+
         if ($lastBlog) {
             $blogs = $blogs->where('id', '!=', $lastBlog->id);
         }
+
         $blogs = $blogs->paginate(6);
 
         return view('pages.home', compact('categories', 'lastBlog', 'blogs'));
