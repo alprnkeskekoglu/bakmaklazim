a478fb3d90761e3c9e1891ea3217eefd64fdb04f
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-04-21 21:45:52 +0300

    tasarımsal revizeler

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,8 +7,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <a class="navbar-brand" href="{{ route('index') }}">
-                <img class="logo_light" src="/assets/images/logo_white.png" alt="logo" />
-                <img class="logo_dark" src="/assets/images/logo_dark.png" alt="logo" />
+                <img class="logo_dark" src="/assets/images/logo.png" alt="logo" />
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="ion-android-menu"></span> </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
