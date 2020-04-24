d9acde3dc294ad51b570fcd163b3d257a52c9545
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-04-24 03:19:50 +0300

    some fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 <head>
 
     <title>@yield('title')</title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="@yield('description')">
