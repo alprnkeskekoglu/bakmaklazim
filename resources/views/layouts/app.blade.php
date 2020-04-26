caaa3f35586c54732b5d12caa42202acfaa0cda8
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-04-27 00:10:27 +0300

    adding share on social media

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,14 +32,12 @@
 
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset("/assets/images/favicon.png") }}">
 
-    <link rel="stylesheet" href="{!! asset("assets/css/animate.css") !!}">
-    <link rel="stylesheet" href="{!! asset("assets/bootstrap/css/bootstrap.min.css") !!}">
-    <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="{!! asset("assets/bootstrap/css/bootstrap.min.css") !!}">
     <link rel="stylesheet" href="{!! asset('/assets/css/all.min.css') . "?v=1" !!}">
-    <link rel="stylesheet" href="{!! asset('/assets/css/style.css'). "?v=9" !!}">
-    <link rel="stylesheet" href="{!! asset('/assets/css/responsive.css'). "?v=3" !!}">
+    <link rel="stylesheet" href="{!! asset('/assets/css/style.min.css'). "?v=1" !!}">
+    <link rel="stylesheet" href="{!! asset('/assets/css/responsive.min.css'). "?v=1" !!}">
     <link rel="stylesheet" href="{!! asset('/assets/css/custom.css'). "?v=8" !!}">
 
     <script type="application/ld+json">
