8ca64480c5e91b853584203452f4d9a09d4588cf
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-04-29 21:34:19 +0300

    some fixes

--- packages/dawnstar/dawnstar/src/Http/Controllers/HomeController.php
+++ packages/dawnstar/dawnstar/src/Http/Controllers/HomeController.php
@@ -3,12 +3,17 @@
 namespace Dawnstar\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Analytics;
+use Spatie\Analytics\Period;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
+        $analyticsData = Analytics::getAnalyticsService()->data_realtime->get(env('ANALYTICS_VIEW_ID'), 'rt:activeUsers');
+
+        dd($analyticsData);
         return view('Dawnstar::pages.home');
     }
 }
